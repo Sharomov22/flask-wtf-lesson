3ca5ce7960a2d1b23f8f986d7880fc8e7c6ab788
Sharomov22 <artemnodvel22@gmail.com>
2025-03-04 18:31:33 +0500

    Задача "Тренировки в полёте"

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ def index(page_name):
     return render_template('base.html', title=page_name)
 
 
-@app.route('/')
-def default():
-    return 'Миссия Колонизация Марса'
-
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', profession=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
